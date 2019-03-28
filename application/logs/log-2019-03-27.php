303e3ee52184f3c2ae43e65cefc4dc8f298cf84d
ferhatbostanci <ferhatbostanciofficial@gmail.com>
2019-03-28 17:42:22 +0300

    Core MVC removed, Codeigniter uploaded

---
+++ application/logs/log-2019-03-27.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-03-27 17:50:39 --> 404 Page Not Found: Sa/index
+ERROR - 2019-03-27 17:52:27 --> 404 Page Not Found: Sa/index
+ERROR - 2019-03-27 17:59:38 --> 404 Page Not Found: Css/test.css
+ERROR - 2019-03-27 17:59:40 --> 404 Page Not Found: Css/index
+ERROR - 2019-03-27 20:39:48 --> PDO: Invalid or non-existent subdriver
+ERROR - 2019-03-27 20:39:48 --> 404 Page Not Found: Faviconico/index
+ERROR - 2019-03-27 20:40:14 --> PDO: Invalid or non-existent subdriver
+ERROR - 2019-03-27 20:40:14 --> PDO: Invalid or non-existent subdriver
+ERROR - 2019-03-27 20:40:15 --> PDO: Invalid or non-existent subdriver
+ERROR - 2019-03-27 20:40:36 --> PDO: Invalid or non-existent subdriver
---
+++ application/logs/log-2019-03-28.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-03-28 14:40:13 --> 404 Page Not Found: Old/users
